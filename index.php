2e6ab2e9781d033e79f94c9f07d3b3278043ad07
Corentin <corentin.mattern@epitech.eu>
2022-06-11 20:22:48 +0200

    php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
+    session_start();
     require_once("twitter.php");
-    if (isset($_POST['submit'])) {
-        publish_tweet();
+    if (isset($_GET['delete_tweet'])) {
+        delete_tweet();
+    }
+    $tweets = [];
+    if (isset($_GET['search'])) {
+        $tweets = search_tweet();
+    } else {
+        $tweets = get_tweets();
     }
 ?>
 <!DOCTYPE html>
@@ -10,6 +17,27 @@
 
     </head>
     <body>
+		<?php
+            if (isset($_SESSION['errors'])) {
+                foreach ($_SESSION['errors'] as $error) {
+                    echo $error . "<br>";
+                }
+            } else if (isset($_SESSION['success'])) {
+                foreach ($_SESSION['success'] as $success) {
+                    echo $success . "<br>";
+                }
+            }
+            session_unset();
+			if (isset($_POST['submit'])) {
+				publish_tweet();
+			}
+		?>
+        <form method="GET" action="./index.php">
+            <label for="search" name="search">Recherche:</label><br>
+            <input type="text" name="search" id="search"><br>
+            <input type="submit" id="submit" value="Envoyer">
+        </form>
+        <hr>
         <form method="POST" action="./index.php">
             <label for="tweet" name="username">Nom d'utilisateur:</label><br>
             <input type="text" name="username" id="username"><br>
@@ -17,5 +45,31 @@
             <textarea id="tweet" name="tweet" cols="50" rows="10"></textarea><br>
             <input type="submit" name="submit" id="submit" value="Envoyer">
         </form>
+        <hr>
+		<?php
+            $count = count($tweets);
+            if ($count > 0) {
+                ?>
+                <p class="result"><?php echo $count; ?> résultats.</p>
+                <hr>
+                <?php
+                foreach ($tweets as $tweet) {
+                    $id = $tweet['id'];
+                    $username = "@".$tweet['username'];
+                    $message = $tweet['message'];
+                    $post_time = "Posté par ". $username ." le ". date("d/m/Y à H:i", strtotime($tweet['time']));
+                    ?>
+                        <div class="tweet" id="tweet-<?php echo $id; ?>">
+                            <p class="message"><?php echo $message; ?></p>
+                            <p class="post_time"><?php echo $post_time; ?></p>
+                            <a href="./index.php?delete_tweet=<?php echo $id; ?>">Supprimer le tweet ?</a>
+                        </div>
+                        <hr>
+                    <?php
+                }
+            } else {
+                echo "Pas de tweets pour le moment ...";
+            }
+		?>
     </body>
 </html>
--- twitter.php
+++ twitter.php
@@ -3,7 +3,10 @@
 require_once("pdo.php");
 
 function get_tweets() {
-    return [];
+	global $database;
+	$req = $database->prepare("SELECT * FROM tweet ORDER BY time DESC");
+	$req->execute();
+	return $req->fetchAll();
 }
 
 function publish_tweet() {
@@ -11,18 +14,48 @@ function publish_tweet() {
     $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
     $tweet = filter_var(trim($_POST['tweet']), FILTER_SANITIZE_STRING);
     if (empty($username)) {
-        echo "Erreur: Le nom d'utilisateur est obligatoire.";
+        echo "Le nom d'utilisateur est obligatoire.";
     } else if (empty($tweet)) {
-        echo "Erreur: Le tweet est obligatoire.";
+        echo "Le tweet est obligatoire.";
+	} else if (strlen($username) > 280) {
+        echo "Le nom d'utilisateur doit faire maximum 255 caractères.";
+	} else if (strlen($tweet) > 280) {
+        echo "Le tweet doit faire maximum 280 caractères.";
     } else {
-        $req = $database->prepare("INSERT INTO tweet (username, message, date), VALUES (:username, :tweet, NOW())");
-        $req->bindParam(":username", $username);
-        $req->bindParam(":tweet", $tweet);
-        $req->execute();
-        #$req->execute(array(
-        #    ":username" => $username,
-        #    ":tweet", $tweet
-        #));
-        echo "Tweet posté !";
+        $req = $database->prepare("INSERT INTO tweet (username, message, time) VALUES (:username, :tweet, NOW())");
+        $req->execute(array(
+            ":username" => $username,
+            ":tweet" => $tweet
+        ));
+        echo "Le tweet a bien été ajouté !";
+    }
+}
+
+function delete_tweet() {
+    global $database;
+    $id = $_GET['delete_tweet'];
+    if (!is_numeric($id)) {
+        echo "Le tweet à supprimer n'est pas valide.";
+    } else {
+        $req = $database->prepare("DELETE FROM tweet WHERE id=:id");
+        $req->execute(array(
+            ":id" => $id
+        ));
+        echo "Le tweet a bien été supprimé !";
+    }
+}
+
+function search_tweet() {
+    global $database;
+    $search = filter_var(trim($_GET['search']), FILTER_SANITIZE_STRING);
+    if (empty($search) ||strlen($search) < 2) {
+        echo "Le tweet recherché doit faire au moins 2 caractères.";
+        return array();
+    } else {
+        $req = $database->prepare("SELECT * FROM tweet WHERE message LIKE :query");
+        $req->execute(array(
+            ":query" => "%".$search."%"
+        ));
+        return $req->fetchAll();
     }
 }
